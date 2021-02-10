fa9da0480d52929182bdfb7983e4293438129633
mateuszral <mateuszral@onet.pl>
2021-02-10 12:31:21 +0100

    Update Paragraph component

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -4,6 +4,10 @@ const Paragraph = styled.p`
   font-weight: ${({ theme, bold }) => bold && theme.font.weight.semiBold};
   color: ${({ theme, grey }) => grey && theme.grey100};
   margin: 5px 0;
+
+  ${({ theme }) => theme.mq.tablet} {
+    margin: 15px 0;
+  }
 `;
 
 export default Paragraph;
