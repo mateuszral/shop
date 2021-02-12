38a2c985d01214c9884e819298daa7d846224afe
mateuszral <mateuszral@onet.pl>
2021-02-12 21:24:59 +0100

    Add helper functions

---
+++ src/helpers/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:1337/';
+
+export const fetchData = async (name) => {
+  try {
+    const { data } = await axios.get(`${baseURL}${name}`);
+    return data;
+  } catch (err) {
+    return err;
+  }
+};
+
+export const getTotalPrice = (cart) => {
+  let totalPrice = 0;
+  cart.forEach(({ price, quantity }) => {
+    totalPrice += price * quantity;
+  });
+  return totalPrice.toFixed(2);
+};
